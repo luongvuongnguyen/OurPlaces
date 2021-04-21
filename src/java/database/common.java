4aa6ba917ac427e28b732b47c17a698b411ea5d4
Luong Vuong Nguyen <51523627+luongvuongnguyen@users.noreply.github.com>
2021-04-21 12:13:16 +0900

    Update common.java

--- src/java/database/common.java
+++ src/java/database/common.java
@@ -2,6 +2,6 @@ package database;
 public interface common {
     public static final String driver="com.mysql.jdbc.Driver";
     public static final String url="jdbc:mysql://localhost:3306/tourism?&useUnicode=true&characterEncoding=UTF-8";
-    public static final String username="root";
-    public static final String password="kelab5136";
+    public static final String username="secrets";
+    public static final String password="secrets";
 }
